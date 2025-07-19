e62eb7e7bbcdf0a70bdda0e3972582469ecd3e7a
Abdulloh <parij4320@gmail.com>
2025-07-19 08:01:09 +0500

    2:4 js

--- index.js
+++ index.js
@@ -15,9 +15,30 @@
 // console.log(repetion("muhammad"));
 
 // 2:2
-let age = [{ age: 20 }, { age: 15 }, { age: 22 }, { age: 33 }];
-age.sort((a,b) => {
-  return(a.age === b.age && a.name.localeCompare(b.name) || a.age - b.age)
-});
+// let age = [{ age: 20 }, { age: 15 }, { age: 22 }, { age: 33 }];
+// age.sort((a,b) => {
+//   return(a.age === b.age && a.name.localeCompare(b.name) || a.age - b.age)
+// });
 
-console.log(age);
+// console.log(age);
+
+//2:3 js
+// for(let i = 1; i <= 20; i++) {
+//   if(i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if(i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+//2:4 js
+// let arr = [1, 5, 7, 9];
+// if(arr.indexOf(7) !== -1) {
+//   console.log(arr.indexOf(7));
+// } else {
+//   console.log(-1);
+// }
